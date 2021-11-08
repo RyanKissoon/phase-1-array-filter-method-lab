0946d9a0c8d8ef86c825259a27270431add182a2
ryankissoon <ryankissoon_31@msn.com>
2021-11-08 08:07:19 -0500

    commit tree 87c806d704ab0d7a5db42a231905a971b53d56c4

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter( driver => driver.toLowerCase() === string.toLowerCase() )
+  }
+  
+function fuzzyMatch(driversName, string){
+    return driversName.filter( driver => driver.indexOf(string) === 0 )
+}
+function matchName(drivers, string) {
+    return drivers.filter( driver => driver.name=== string );
+}
